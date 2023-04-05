e053c7ceb95b932e7d2dc98fccd19fd533265f6d
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-04-05 16:38:54 +0200

    inserito  onclick

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
+function myFunction(){
+    document.getElementById("demo").innerHTML = "mi hai cliccato";
 
-let nome = prompt ("inserisci il tuo nome?");
-// console.log (`il nome dell'utente è ${nome}`);
+    let nome = prompt ("inserisci il tuo nome?");
+    // console.log (`il nome dell'utente è ${nome}`);
 
-let cognome = prompt ("Inserisci il tuo cognome");
-// console.log (`il cognome dell'utente è ${cognome}`);
+    let cognome = prompt ("Inserisci il tuo cognome");
+    // console.log (`il cognome dell'utente è ${cognome}`);
 
-let colore = prompt ("inserisci il tuo colore preferito");
-// console.log (`il colore preferito dell'utente è ${colore}`);
+    let colore = prompt ("inserisci il tuo colore preferito");
+    // console.log (`il colore preferito dell'utente è ${colore}`);
 
-let password = `${nome}${cognome}${colore}`;
+    let password = `${nome}${cognome}${colore}`;
 
-document.getElementById("psDaCambiare").innerText = password + 2023;
+    document.getElementById("psDaCambiare").innerHTML = password + 2023;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
