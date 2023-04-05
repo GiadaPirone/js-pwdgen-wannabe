ffa48e4f7cf9f9b2fb9ac107e25b47fe7711bfe7
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-04-05 15:36:41 +0200

    creazione password

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+let nome = prompt ("inserisci il tuo nome?");
+// console.log (`il nome dell'utente è ${nome}`);
+
+let cognome = prompt ("Inserisci il tuo cognome");
+// console.log (`il cognome dell'utente è ${cognome}`);
+
+let colore = prompt ("inserisci il tuo colore preferito");
+// console.log (`il colore preferito dell'utente è ${colore}`);
+
+let password = `${nome}${cognome}${colore}`;
+
+document.getElementById("psDaCambiare").innerText = password + 2023;
+
+
+
+
+
+
+
